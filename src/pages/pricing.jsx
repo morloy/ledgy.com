2f2e80ddb851218fd18195489849729600e966bc
koyanoa <yosp@bluewin.ch>
2019-01-23 16:47:47 +0100

    First draft

--- src/pages/pricing.jsx
+++ src/pages/pricing.jsx
@@ -36,9 +36,6 @@ export default withI18n()(({ i18n, ...props }: Props) => (
             <p>
               <Trans>
                 Ledgy scales with your needs. Free for startups, powerful for grown-ups.
-                It’s currently free, nevertheless, this probably changes soon.
-                If you sign up now, we’ll make sure, that our loyal first users will
-                get a good deal.
               </Trans>
             </p>
           </header>
@@ -50,27 +47,33 @@ export default withI18n()(({ i18n, ...props }: Props) => (
             <div className="col-lg-4">
               <div className="pricing-3 border rounded">
                 <h6><Trans>Startup</Trans></h6>
-                <h2 className="price mb-0"><Trans>Free</Trans></h2>
+                <h2><Trans>Free</Trans></h2>
                 <small className="text-muted">&nbsp;</small>
                 <ul className="text-left mt-6 mb-0">
-                  <Li><Trans>Up to 15 shareholders</Trans></Li>
                   <Li><Trans>Error-free cap table</Trans></Li>
                   <Li><Trans>Round modeling</Trans></Li>
+                  <Li><Trans>Convertibles</Trans></Li>
+                  <Li><Trans>Access rights</Trans></Li>
                 </ul>
+                <br />
+                <a className="btn btn-round btn-outline-primary w-200" href="#try">Get started</a>
               </div>
             </div>
 
 
             <div className="col-lg-4">
-              <div className="pricing-3 border rounded">
-                <h6><Trans>Business</Trans></h6>
-                <h2 className="price mb-0"><Trans>€2</Trans></h2>
-                <small className="text-muted"><Trans>per shareholder per month</Trans></small>
+              <div className="pricing-3 popular border rounded">
+                <h6><Trans>Pro</Trans></h6>
+                <h2><Trans>€79/month</Trans></h2>
+                <small className="text-muted"><Trans>€99/month if billed monthly</Trans></small>
                 <ul className="text-left mt-6 mb-0">
-                  <Li><Trans>All basic features</Trans></Li>
-                  <Li><Trans>Unlimited shareholders</Trans></Li>
-                  <Li><Trans>Incentive plan management</Trans></Li>
+                  <Li><Trans>All free features</Trans></Li>
+                  <Li><Trans>Employee participation plans</Trans></Li>
+                  <Li><Trans>Email notifications</Trans></Li>
+                  <Li><Trans>Exit modeling</Trans></Li>
                 </ul>
+                <br />
+                <a className="btn btn-round btn-primary w-200" href="#try">Get started</a>
               </div>
             </div>
 
@@ -78,13 +81,16 @@ export default withI18n()(({ i18n, ...props }: Props) => (
             <div className="col-lg-4">
               <div className="pricing-3 border rounded">
                 <h6><Trans>Enterprise</Trans></h6>
-                <h2 className="price mb-0"><Trans>€4</Trans></h2>
-                <small className="text-muted"><Trans>per shareholder per month</Trans></small>
+                <h2><Trans>Contact us</Trans></h2>
+                <small className="text-muted">&nbsp;</small>
                 <ul className="text-left mt-6 mb-0">
-                  <Li><Trans>All premium features</Trans></Li>
-                  <Li><Trans>Exit modeling</Trans></Li>
+                  <Li><Trans>All pro features</Trans></Li>
+                  <Li><Trans>Grant automation</Trans> <span className="badge badge-pill badge-success badge-number badge-pale ml-1">soon</span></Li>
+                  <Li><Trans>Digital signatures</Trans> <span className="badge badge-pill badge-success badge-number badge-pale ml-1">soon</span></Li>
                   <Li><Trans>Free cap table import</Trans></Li>
                 </ul>
+                <br />
+                <a className="btn btn-round btn-outline-primary w-200" href="#try">Get started</a>
               </div>
             </div>
 
