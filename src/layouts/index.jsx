773d8988faca2863717d5bd2c301785f5cf4899f
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-02-04 15:07:26 +0100

    create styles folder and import bootstrap + old variables

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -4,10 +4,12 @@ import React, { useEffect, type Node } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import { I18nProvider, withI18n } from '@lingui/react';
 import { Helmet } from 'react-helmet';
+
 import 'typeface-roboto-slab'; // eslint-disable-line import/extensions
 import 'typeface-work-sans'; // eslint-disable-line import/extensions
 import 'katex/dist/katex.min.css';
 import 'prism-themes/themes/prism-ghcolors.css';
+import '../styles/_index.scss';
 
 import { name, loadScript, animateTablet, isDevelopment, ContentSecurityPolicy } from '../helpers';
 import { Title } from './utils';
