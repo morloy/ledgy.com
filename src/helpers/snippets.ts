b94bcc5d03ee0a7bc9d92ba21ab08321de1e943b
Timo Horstschaefer <mail@timo-horstschaefer.de>
2021-06-11 00:10:28 +0200

    fix resolve

--- src/helpers/snippets.ts
+++ src/helpers/snippets.ts
@@ -58,6 +58,7 @@ export const loadSegment = (integrations: Record<string, boolean>): Promise<void
         analytics.ready(resolve);
         analytics.page();
       }
+    else resolve();
   })();
 });
 
